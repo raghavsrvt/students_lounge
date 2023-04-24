21f27bb0f22c8ac9cc01eafffc2836a1e29736d2
= <=>
2023-04-24 20:58:21 +0530

    added memes as rewards in stop watch

--- stop-watch/app.js
+++ stop-watch/app.js
@@ -5,6 +5,10 @@ let startBtn = document.querySelector('.start');
 let stopBtn = document.querySelector('.stop');
 let resetBtn = document.querySelector('.reset');
 
+let achivementDiv = document.querySelector('.achievement');
+let achievementHr = document.querySelector('.ach-hr');
+let achievementImg = document.querySelector('.ach-img');
+
 let secNum = 0;
 let minNum = 0;
 let hourNum = 0;
@@ -27,6 +31,7 @@ var startBtnInterval;
 
 startBtn.addEventListener('click', () => {
     startBtnInterval = setInterval(function () {
+        achievementDisplay();
         stopBtn.removeAttribute('disabled');
         resetBtn.setAttribute('disabled', 'disabled');
         secNum += 1;
@@ -88,4 +93,75 @@ const addHour = ()=>{
     else{
         hours.innerHTML = hourNum;
     }
+}
+
+const achievementDisplay = () =>{
+    if(hourNum == 0 && minNum == 30){
+        if(secNum == 0){
+            achivementDiv.classList.toggle('hidden');
+            achievementHr.innerText = "30 minutes";
+            achievementImg.setAttribute('src' , '../img/stop-watch/1.png');
+        }
+        if(secNum == 6){hidden();}
+    }
+    else if(hourNum == 1 && minNum == 0){
+        if(secNum == 0){
+            achivementDiv.classList.toggle('hidden');
+            achievementHr.innerText = "1 Hour";
+            achievementImg.setAttribute('src' , '../img/stop-watch/2.png');
+        }
+        if(secNum == 6){hidden();}
+    }
+    else if(hourNum == 1 && minNum == 30){
+        if(secNum == 0){
+            achivementDiv.classList.toggle('hidden');
+            achievementHr.innerText = "1hr 30min";
+            achievementImg.setAttribute('src' , '../img/stop-watch/3.png');
+        }
+        if(secNum == 6){hidden();}
+    }
+    else if(hourNum == 2 && minNum == 0){
+        if(secNum == 0){
+            achivementDiv.classList.toggle('hidden');
+            achievementHr.innerText = "2 Hours";
+            achievementImg.setAttribute('src' , '../img/stop-watch/4.png');
+        }
+        if(secNum == 6){hidden();}
+    }
+    else if(hourNum == 2 && minNum == 30){
+        if(secNum == 0){
+            achivementDiv.classList.toggle('hidden');
+            achievementHr.innerText = "2hr 30min";
+            achievementImg.setAttribute('src' , '../img/stop-watch/5.png');
+        }
+        if(secNum == 6){hidden();}
+    }
+    else if(hourNum == 3 && minNum == 0){
+        if(secNum == 0){
+            achivementDiv.classList.toggle('hidden');
+            achievementHr.innerText = "3 hours";
+            achievementImg.setAttribute('src' , '../img/stop-watch/6.png');
+        }
+        if(secNum == 6){hidden();}
+    }
+    else if(hourNum == 4 && minNum == 0){
+        if(secNum == 0){
+            achivementDiv.classList.toggle('hidden');
+            achievementHr.innerText = "4 Hours";
+            achievementImg.setAttribute('src' , '../img/stop-watch/7.png');
+        }
+        if(secNum == 6){hidden();}
+    }
+    else if(hourNum == 5 && minNum == 0){
+        if(secNum == 0){
+            achivementDiv.classList.toggle('hidden');
+            achievementHr.innerText = "5 Hours";
+            achievementImg.setAttribute('src' , '../img/stop-watch/8.png');
+        }
+        if(secNum == 6){hidden();}
+    }
+};
+
+const hidden = ()=>{
+    achivementDiv.classList.toggle('hidden');
 }
